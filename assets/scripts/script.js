fe60d0ee7438bd62aea6ef84e9811aa9fd83cd5b
Steve Diabo <stevediabo@gmail.com>
2021-01-21 17:20:34 -0500

    d

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -209,8 +209,14 @@ $(function()
 		});
 
 		// if form has errors
-		if (errorState)
+		if (errorState) {
+
+			// unlock disable button
+			$form.find('.jc-btn-submit').prop('disabled', false);
+
 			return false;
+
+		}
 
 		// now we do ajax
 		// get form
@@ -257,181 +263,6 @@ $(function()
 			}
 		});
 
-	});
-
-});
-
-
-//----------------------------------------------------------------------------//
-// Footer Mailing List Form                                                   //
-//----------------------------------------------------------------------------//
-
-$(function()
-{
-
-	////////////////////////////////////////////////////////////////////////////////
-
-	var $form              = $('#oj-footer-form');
-	var $email             = $form.find ('.form-control[name=Email]');
-	var $checkbox          = $form.find ('.form-check-input[name=consent]');
-	var $button            = $form.find ('.oj-btn-submit');
-	var errorState         = false;
-
-	////////////////////////////////////////////////////////////////////////////////
-
-	function isEmail(email) {
-
-		var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-
-		if(!regex.test(email))
-		{
-			return false;
-		}
-
-		else
-		{
-			return true;
-		}
-	}
-
-	$form.submit (function(e)
-	{
-		// prevent default submit behaviour
-		e.preventDefault();
-
-		// prevent duplicate submissions
-		$form.find('.oj-btn-submit').prop('disabled', true);
-
-		// reset error state
-		errorState = false;
-
-		// check whether email is valid
-		if (!isEmail ($email.val()))
-		{
-			$email.removeClass ('is-valid').addClass ('is-invalid');
-			errorState = true;
-		}
-
-		else
-		{
-			$email.removeClass ('is-invalid').addClass ('is-valid');
-		}
-
-		// check whether consent checkbox is checked
-		if ($checkbox.prop('checked')!=true)
-		{
-			$checkbox.removeClass ('is-valid').addClass ('is-invalid');
-			errorState = true;
-		}
-		else
-		{
-			$checkbox.removeClass ('is-invalid');
-		}
-
-		// if form has errors
-		if (errorState) {
-
-			// unlock disable button
-			$form.find('.oj-btn-submit').prop('disabled', false);
-
-			return false;
-
-		}
-
-		// now we do ajax
-		// get form
-		var form = $('#oj-footer-form')[0];
-
-		// create an FormData object 
-		var data = new FormData(form);
-
-		// do a barrel roll
-		$.ajax({
-			type: "POST",
-			enctype: 'multipart/form-data',
-			url: "/register.php",
-			data: data,
-			processData: false,
-			contentType: false,
-			cache: false,
-			timeout: 800000,
-			success: function (data) {
-
-				$form.addClass('oj-success');
-
-			},
-			error: function (e) {
-
-				console.log("ERROR : ", e);
-
-				$form.addClass('oj-error');
-
-			}
-		});
-
-	});
-
-});
-
-//----------------------------------------------------------------------------//
-// ScrollMagic Animation incl. Lazy Load                                      //
-//----------------------------------------------------------------------------//
-
-$(function()
-{
-
-	function lazyLoad() {
-
-		$('.ec-lazy').each(function () {
-
-			var $this = $(this);
-
-			new ScrollMagic.Scene
-			({
-				triggerElement: this,
-				triggerHook: 1,
-				offset: -100
-			})
-			.on("enter", function () {
-				$this.removeClass('ec-lazy');
-				$this.attr({
-					src:    $this.attr('data-src'),
-					srcset: $this.attr('data-srcset'),
-					poster: $this.attr('data-poster')
-				});
-				$this.removeAttr('data-src data-srcset data-poster');
-			})
-			.addTo(controller);
-		});
-	};
-
-	function stickyNav() {
-
-		new ScrollMagic.Scene
-		({
-			triggerElement: 'body', triggerHook: 0, offset: 60
-		})
-		.setClassToggle('.oj-navbar', 'oj-shown')
-		.addTo(controller);
-
-	}
-
-	// Create an instance of the Scroll Magic Controller
-	let controller = new ScrollMagic.Controller();
-
-	lazyLoad();
-	stickyNav();
-
-	$('.ec-animated-section').each(function () {
-
-		var $this = $(this);
-
-		new ScrollMagic.Scene
-		({
-			triggerElement: this, triggerHook: 0.5, reverse: false
-		})
-		.setClassToggle(this, 'ec-animated')
-		.addTo(controller);
 	});
 
 });
